1470329b12a8ec44d2a2d1c1020c681bdd467b00
Iskander Caracoza <serjein12@gmail.com>
2022-11-11 20:16:08 -0600

    se modifica JS

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,84 @@
-// function filtrarDiscos(){
-//     if(inputBuscar.value.trim() !== "" ){
-// let resultado = discos.filter(disco => disco.artista.toUpperCase().includes(inputBuscar.value.toUpperCase().trim()))
-// if (resultado.length > 0){
-//    console.table(resultado)
-   
-//    let totalCompra = resultado.reduce((total, disco) => total + disco.importe, 0)
-//     console.log("El total de la compra es: $", totalCompra)
-// }   else{
-//     alert("No se encontro artista")
-// } 
-// }
-// }
+const mensajeInicial = "💽 Selecciona el album con el código:"
 
-// const inputBuscar = prompt("Indica el artista a buscar")
+function buscarAlbum(id) {
+    let resultado = albums.find(album => album.id === parseInt(id))
+    return resultado
+}
 
-function buscarArtista(){
-    let art = prompt("Ingresa Artista a buscar:💽").toUpperCase()
- 
-    let resultado = discos.filter((disco) => disco.artista.toUpperCase().includes(art))
-    if(resultado.length > 0){
-        console.table(resultado)
-   
-           let totalCompra = resultado.reduce((total, disco) => total + disco.importe, 0).toFixed(2)
-            alert("El total de la compra es: $ " + totalCompra)
-           
-    }else{
-        alert("⛔ No se encontro el Artista", resultado)
+function comprar() {
+    let id = prompt(mensajeInicial)
+    if (!parseInt(id)) {
+        alert("🤷‍♂️ Error en el código ingresado.")
+        return
+    }
+    let albumElegido = buscarAlbum(id)
+    carrito.push(albumElegido)
+    let respuesta = confirm("¿Deseas llevar otro album?")
+    if (respuesta) {
+        comprar()
+    } else {
+        finalizarCompra()
+    }
+}
+
+function verCarrito() {
+    if (carrito.length > 0) {
+        console.table(carrito)
+
+        let totalCompra = carrito.reduce((total, album) => total + album.importe, 0).toFixed(2)
+             console.log("El total de la compra es: $ " + totalCompra)
+    } else {
+        console.warn("El carrito está vacío!")
+    }
+}
+
+function finalizarCompra() {
+    if (carrito.length === 0) {
+        console.warn("El carrito está vacío!")
+        return
+    }
+    const compras = new Compra(carrito)
+    alert(`El costo total es de $ ${compras.obtenerSubtotal()}`)
+    let respuesta = confirm("¿Deseas confirmar tu compra?")
+    if (respuesta) {
+        alert(compras.confirmarCompra())
+        carrito.length = 0
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function buscarArtista(){
+//     let art = prompt("Ingresa Artista a buscar:💽").toUpperCase()
+
+//     let resultado = discos.filter((disco) => disco.artista.toUpperCase().includes(art))
+//     if(resultado.length > 0){
+//         console.table(resultado)
+
+//            let totalCompra = resultado.reduce((total, disco) => total + disco.importe, 0).toFixed(2)
+//             alert("El total de la compra es: $ " + totalCompra)
+
+//     }else{
+//         alert("⛔ No se encontro el Artista", resultado)
+//     }
+// }
--- JS/variables.js
+++ JS/variables.js
@@ -1,13 +1,30 @@
-const discos = [{id: 1234, artista: "Korn", album: "Issues", importe: 50},
-                   {id: 5678, artista: "System of a Down", album: "Toxicity", importe: 143},
-                   {id: 9012, artista: "Muse", album: "The 2nd law", importe: 154.50},
-                   {id: 3456, artista: "Slipknot", album: "All Hope Is Gone", importe: 128.50},
-                   {id: 7890, artista: "Korn", album: "Follow the Leader", importe: 84.50},
-                   {id: 9235, artista: "System of a Down", album: "Mezmerize", importe: 60},
-                   {id: 5968, artista: "Muse", album: "Simulation Theory", importe: 70},
-                   {id: 5968, artista: "Slipknot", album: "The Subliminal Verses", importe: 50},]
+const carrito = []
 
-                   
+const albums = [{id: 1, artista: "Korn", album: "Issues", importe: 50},
+                   {id: 2, artista: "System of a Down", album: "Toxicity", importe: 143},
+                   {id: 3, artista: "Muse", album: "The 2nd law", importe: 154.50},
+                   {id: 4, artista: "Slipknot", album: "All Hope Is Gone", importe: 128.50},
+                   {id: 5, artista: "Korn", album: "Follow the Leader", importe: 84.50},
+                   {id: 6, artista: "System of a Down", album: "Mezmerize", importe: 60},
+                   {id: 7, artista: "Muse", album: "Simulation Theory", importe: 70},
+                   {id: 8, artista: "Slipknot", album: "The Subliminal Verses", importe: 50},]
 
-
-                  
+class Compra {
+    constructor(carrito) {
+        this.carrito = carrito
+    }
+    obtenerSubtotal() {
+        if (carrito.length > 0) {
+            return this.carrito.reduce((subTotal, album) => subTotal + album.importe, 0).toFixed(2)
+        } else {
+            return 'No hay datos'
+        }
+    }
+    confirmarCompra() {
+        if (this.obtenerSubtotal() !== 'No hay datos') {
+            return `👍 Confirmamos el pago de $ ${this.obtenerSubtotal()}. \n Gracias por su compra, vuelva pronto!`
+        } else {
+            return `🤦‍♂️ Error en la compra.`
+        }
+    }
+}
